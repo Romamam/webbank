f1cf15fed82f37977b7e4a51a1f424a4c13da49b
Roman Rybachok <roman.ribachok@gmail.com>
2024-07-31 21:55:06 +0300

    first commit

---
+++ src/test/java/app/webbank/WebBankApplicationTests.java
@@ -0,0 +1,13 @@
+package app.webbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
