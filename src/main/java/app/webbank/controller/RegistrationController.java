49f74bfb1c2306cd0b6f1b22647a5e62ccc056fb
Roman Rybachok <roman.ribachok@gmail.com>
2024-08-14 15:17:04 +0300

    new - added default categories

--- src/main/java/app/webbank/controller/RegistrationController.java
+++ src/main/java/app/webbank/controller/RegistrationController.java
@@ -1,25 +1,45 @@
 package app.webbank.controller;
 
+import app.webbank.dao.CategoryRepository;
 import app.webbank.dao.UserRepository;
+import app.webbank.model.Category;
 import app.webbank.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RegistrationController {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final CategoryRepository categoryRepository;
+
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder, CategoryRepository categoryRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.categoryRepository = categoryRepository;
+    }
 
     @PostMapping("/sign-up/user")
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("USER");
-        return userRepository.save(user);
+
+        User savedUser = userRepository.save(user);
+
+        List<Category> defaultCategories = List.of(
+                new Category("Food", savedUser),
+                new Category("Transport", savedUser),
+                new Category("Entertainment", savedUser),
+                new Category("Utilities", savedUser)
+        );
+
+        categoryRepository.saveAll(defaultCategories);
+
+        return savedUser;
     }
 }
